2c5fcdd35f90124bcb689fa93ff6752cab2ee9b9
Ryna <pinktulips@Hotmail.com>
2021-07-21 10:40:28 -0700

    form issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,7 @@ class PostsController < ApplicationController
 
   def create 
     @post = Post.new(post_params)
-    if @post.valid?
-      @post.save
+    if @post.save
       redirect_to post_path(@post)
     else
       render :new
@@ -23,7 +22,8 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    if @post.update(post_params)
+    @post.update(post_params)
+    if @post.save
       redirect_to post_path(@post)
     else
       render :edit
