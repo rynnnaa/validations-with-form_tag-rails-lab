5dc385a66643c8c7eadfdb6bcb295efcc1a07d8e
Ryna <pinktulips@Hotmail.com>
2021-07-21 09:34:55 -0700

    example 18

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,16 +3,32 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new 
+    @post = Post.new(post_params)
+  end
+
+  def create 
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
 
   def update
     @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
@@ -20,4 +36,9 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:title, :category, :content)
   end
+
+  def setpost
+    @post = Post.find(params[:id])
+  end
 end
+
